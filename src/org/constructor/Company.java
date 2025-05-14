59e37b64b45190fdc8479d9823498f8554577cc5
harisachin07 <hariharan.ecs@gmail.com>
2025-05-14 11:27:08 +0530

    Hariharan

---
+++ src/org/constructor/Company.java
@@ -0,0 +1,34 @@
+package org.constructor;
+
+//Child Class
+public class Company extends Department{
+	private void name() {
+		System.out.println("Hariharan");
+	}
+		
+		//Create Constructor
+		public Company() {
+			this(51);
+			System.out.println("default Constructor");	
+			
+		}
+
+		public Company(int age) {
+				this("Hello");
+				System.out.println("integer based constructor");	
+		}
+
+		public Company(String name) {
+			super();
+			System.out.println("String Based Constructor");
+			
+		}
+		public static void main(String[] args) {
+			Company co= new Company();
+			co.name();
+		}
+		
+		
+		
+	}
+
---
+++ src/org/constructor/Department.java
@@ -0,0 +1,8 @@
+package org.constructor;
+
+public class Department {
+	public Department() {
+		System.out.println("THIS IS THE PARENT CLASS OF DEFAULT CONSTRUCTOR");
+	}
+
+}
